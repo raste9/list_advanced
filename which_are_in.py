2958fec1f721da79ac931570319c48e40c3e5f04
Petya <145002477+raste9@users.noreply.github.com>
2023-10-23 12:17:48 +0300

    Add files via upload

---
+++ which_are_in.py
@@ -0,0 +1,16 @@
+first_sequence = input().split(', ')
+second_sequence = input().split(', ')
+substrings = []
+
+for first_string in first_sequence:
+    for second_string in second_sequence:
+        if first_string in second_string:
+            substrings.append(first_string)
+            break
+print(substrings)
+
+# input -
+# arp, live, strong
+# lively, alive, harp, sharp, armstrong
+#
+# output ['arp', 'live', 'strong']
