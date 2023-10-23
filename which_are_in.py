cc1c0b18a8f7a2d49b697924d760feeb3f180b1e
Petya <145002477+raste9@users.noreply.github.com>
2023-10-23 12:29:18 +0300

    Update which_are_in.py

--- which_are_in.py
+++ which_are_in.py
@@ -9,6 +9,14 @@ for first_string in first_sequence:
             break
 print(substrings)
 
+----------------------------------------------
+
+first_sequence = input().split(', ')
+second_sequence = input().split(', ')
+
+print([first_string for first_string in first_sequence \
+       if any(first_string in second_string for second_string in second_sequence)])
+
 # input -
 # arp, live, strong
 # lively, alive, harp, sharp, armstrong
